c4c47bd391ff88a40813bbcd29f09dc8d2549246
cwkim723 <jerry.pref@gmail.com>
2022-03-05 12:40:58 +0900

    feat: 객체와 테이블 매핑

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -2,8 +2,10 @@ package hellojpa;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
-@Entity
+@Entity(name = "Member") // jpa가 관리하는 객체, db과 매핑해서 씀 / 기본적으로 name은 클래스 이름으로 쓰기 때문에 굳이 name을 쓸 일은 별로 없음
+@Table(name = "MBR") // 클래스 이름과 db 테이블명이 다를 경우 매핑하기 위해 씀 -> 이걸 쓰면 from MBR로 됨
 public class Member {
 
     @Id
