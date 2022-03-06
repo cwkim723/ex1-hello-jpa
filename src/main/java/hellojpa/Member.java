7afaa9a0ccd1ecc689fb14fb1d50da865d614dd4
cwkim723 <jerry.pref@gmail.com>
2022-03-06 22:07:47 +0900

    feat: 데이터베이스 스키마 자동 생성

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,15 +1,18 @@
 package hellojpa;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity(name = "Member") // jpa가 관리하는 객체, db과 매핑해서 씀 / 기본적으로 name은 클래스 이름으로 쓰기 때문에 굳이 name을 쓸 일은 별로 없음
-@Table(name = "MBR") // 클래스 이름과 db 테이블명이 다를 경우 매핑하기 위해 씀 -> 이걸 쓰면 from MBR로 됨
+//@Table(name = "MBR") // 클래스 이름과 db 테이블명이 다를 경우 매핑하기 위해 씀 -> 이걸 쓰면 from MBR로 됨
 public class Member {
 
     @Id
     private Long id;
+
+    @Column(unique = true, length = 10)
     private String name;
 
     public Member() {}
